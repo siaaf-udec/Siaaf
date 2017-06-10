dcfc6fcfe5f5757ca334c481b9a698d208c927e6
Miguel Ortiz <miguelortiz1994@outlook.com>
2017-06-09 23:46:12 -0500

    csrf-token
    - meta de dashboard.blade.php
    - Issues meta csrf-token tag dashboard @Yaien

--- resources/views/material/layouts/dashboard.blade.php
+++ resources/views/material/layouts/dashboard.blade.php
@@ -10,6 +10,7 @@
     <meta content="width=device-width, initial-scale=1" name="viewport"/>
     <meta content="{{ config( 'app.description' ) }}" name="description"/>
     <meta content="{{ config( 'app.author', 'Siaaf' ) }}" name="author"/>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     @include('material.partials.head')
 
