c5ef63e727d9670bfe669c8ba7b8ce2502a19ba8
Jerson <jersongutierrez1@gmail.com>
2017-07-12 12:49:36 -0500

    Gestion Funcionarios Audiovisuales

--- resources/views/audiovisuals/funcionario/gestionfuncionarios.blade.php
+++ resources/views/audiovisuals/funcionario/gestionfuncionarios.blade.php
@@ -39,23 +39,26 @@
 <script src="{{ asset('assets/global/scripts/datatable.js') }}" type="text/javascript"></script>
 <script src="{{ asset('assets/global/plugins/datatables/datatables.min.js') }}" type="text/javascript"></script>
 <script src="{{ asset('assets/global/plugins/datatables/plugins/bootstrap/datatables.bootstrap.js') }}" type="text/javascript"></script>
+<script src="{{ asset('assets/global/plugins/bootstrap-toastr/toastr.min.js') }}" type="text/javascript"></script>
+
 @endpush
 
 @push('functions')
 <script src="{{ asset('assets/main/scripts/table-datatable.js') }}" type="text/javascript"></script>
+<script src="{{ asset('assets/main/scripts/ui-toastr.js') }}" type="text/javascript"></script>
 <script>
 jQuery(document).ready(function () {
+    //UIToastr.init('success', 'Bien', 'Correcto');
 	var table, url, columns;
 	table = $('#gestionar-funcionarios-ajax');
-	url = "{{ route('funcionario.index') }}";
+	url = "{{ route('funcionario.listing') }}";
 
 	columns = [
         {data: 'DT_Row_Index'},
-        {data: 'id', "visible": false },
-        {data: 'PK_FNS_Cedula', name: 'Nombre'},
-        {data: 'FNS_Nombres', name: 'Correo Electronico'},
+        {data: 'PK_FNS_Cedula', name: 'Cedula'},
+        {data: 'FNS_Nombres', name: 'Nombres'},
         {data: 'FNS_Correo', name: 'Correo Electronico'},
-        {data: 'FK_FNS_Programa', name: 'Correo Electronico'},
+        {data: 'FK_FNS_Programa', name: 'Programa'},
         {
             defaultContent: '<a href="javascript:;" class="btn btn-simple btn-warning btn-icon edit"><i class="icon-pencil"></i></a><a href="javascript:;" class="btn btn-simple btn-danger btn-icon remove"><i class="icon-trash"></i></a>',
             data:'action',
@@ -77,14 +80,24 @@ jQuery(document).ready(function () {
         e.preventDefault();
         $tr = $(this).closest('tr');
         var dataTable = table.row($tr).data();
-        alert(dataTable.id);
+
+        alert(dataTable.PK_FNS_Cedula);
+
+        $.get( "../../audiovisuales/funcionario/all/"+ dataTable.PK_FNS_Cedula, function( data ) {
+            console.log(data);
+            //table.ajax.reload();
+        });
     });
     table.on('click', '.edit', function (e) {
         e.preventDefault();
         $tr = $(this).closest('tr');
         var dataTable = table.row($tr).data();
-        alert(dataTable.id);
+        alert(dataTable.PK_FNS_Cedula);
     });
+    /*$('#ipdate').on('click', function(){
+        var id = $('#id_usu').val();
+    });*/
+
 });
 </script>
 @endpush
--- routes/audiovisuals.php
+++ routes/audiovisuals.php
@@ -3,10 +3,28 @@
  * Audiovisuales
  */
 
-//RUTA DE EJEMPLO
 Route::get('/', [
     'as' => 'audiovisuales.index',
     'uses' => function(){
         return view('audiovisuals.example');
     }
 ]);
+
+
+
+
+Route::group(['prefix' => 'funcionario'], function () {
+    $controller = "\\App\\Container\\Audiovisuals\\Src\\Controllers\\";
+    Route::get('index',[
+	    'uses' => $controller.'FuncionarioController@index',
+	    'as' => 'funcionario.index'
+    ]);
+    Route::get('listing',[
+        'uses' => $controller.'FuncionarioController@listing',
+        'as' => 'funcionario.listing'
+    ]);
+    Route::get('all/{id}',[
+        'uses' => $controller.'FuncionarioController@all',
+        'as' => 'funcionario.all'
+    ]);
+});
