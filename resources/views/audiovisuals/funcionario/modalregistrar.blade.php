e47d2dee338e8a7680afaf3026e7121ac58153b5
Jerson <jersongutierrez1@gmail.com>
2017-07-17 00:37:44 -0500

    Modificacion tabla Funcionarios Audiovisuales

--- resources/views/audiovisuals/funcionario/modalregistrar.blade.php
+++ resources/views/audiovisuals/funcionario/modalregistrar.blade.php
@@ -1,7 +1,7 @@
 
 
 
-<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+<div class="modal fade" id="modal-registrar-funcionario" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
 	<div class="modal-dialog" role="document">
 	<div class="modal-content">
 		<div class="modal-header">
@@ -9,11 +9,11 @@
 			<h4 class="modal-title" id="myModalLabel">Actualizar Funcionario</h4>
 		</div>
 		<div class="modal-body">
-	{!! Form::open (['id' => 'form_funcionario', 'class' => '', 'url' => '/forms']) !!}
+	{!! Form::open (['id' => 'form_funcionario_registrar', 'class' => '', 'url' => '/forms']) !!}
 
 			<input type="hidden" name="_token" value="{{ csrf_token() }}" id="token">{{--IDENTIFICACION DE PETICION PARA LARAVEL --}}
 			<input type="hidden" id="id"> {{-- PARA AJAX --}}
-			@include('audiovisuals.funcionario.forms.fns')
+			@include('audiovisuals.funcionario.forms.fns_registrar')
 		</div>
 		<div class="modal-footer">
 			{{-- {!!link_to('#', $title='Actualizar', $atrributes = ['id'=>'actualizar', 'class'=>'btn green'], $secure = null) !!} --}}
