16020787aba95da844c522ddfb0a4fc9fd99061c
Laura Camila Romero <lauraacamila16@hotmail.com>
2017-07-18 13:49:16 -0500

    Rutas Financial

--- routes/financial.php
+++ routes/financial.php
@@ -3,6 +3,15 @@
  * Financiero.
  */
 //RUTA DE EJEMPLO
+
+Route::group(['prefix' => 'lol'], function () {
+  $controller = "\\App\\Container\\Financial\\Src\\Controllers\\";
+  Route::get('/laura', [
+      'uses' => $controller.'SampleController@index',
+      'as' => 'laura'
+  ]);
+});
+
 Route::get('/', [
     'as' => 'financial.index',
     'uses' => function(){
