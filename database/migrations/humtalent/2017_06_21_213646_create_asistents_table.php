99124d4ee5045bdbf0f9239a1749f86a4667515a
YeisonGomezR <yandrey.yg@gmail.com>
2017-07-20 17:54:34 -0500

    Modulo Eventos-Talento humano
    CRUD Eventos

--- database/migrations/humtalent/2017_06_21_213646_create_asistents_table.php
+++ database/migrations/humtalent/2017_06_21_213646_create_asistents_table.php
@@ -14,8 +14,7 @@ class CreateAsistentsTable extends Migration
     public function up()
     {
         Schema::connection('humtalent')->create('TBL_Asistentes', function (Blueprint $table)  {
-            $table->increments('PK_ASIST_Numero')->unsigned()->unique();
-            $table->integer('ASIST_Informe')->unsigned();
+            $table->String('ASIST_Informe',10);
             $table->integer('FK_TBL_Eventos_IdEvento')->unsigned();
             $table->integer('FK_TBL_Persona_Cedula')->unsigned();
             $table->foreign('FK_TBL_Eventos_IdEvento')->references('PK_EVNT_IdEvento')->on('TBL_Eventos');
--- resources/views/humtalent/eventos/editarEvento.blade.php
+++ resources/views/humtalent/eventos/editarEvento.blade.php
@@ -1,7 +1,5 @@
 @extends('material.layouts.dashboard')
 @push('styles')
-<link href="{{ asset('assets/global/plugins/datatables/datatables.min.css') }}" rel="stylesheet" type="text/css"/>
-<link href="{{ asset('assets/global/plugins/datatables/plugins/bootstrap/datatables.bootstrap.css') }}" rel="stylesheet" type="text/css" />
 <link href="{{ asset('assets/global/plugins/bootstrap-toastr/toastr.css') }}" rel="stylesheet" type="text/css" />
 <link href="{{ asset('assets/global/plugins/bootstrap-datepicker/css/bootstrap-datepicker3.min.css') }}" rel="stylesheet" type="text/css" />
 <link href="{{ asset('assets/global/plugins/bootstrap-timepicker/css/bootstrap-timepicker.min.css') }}" rel="stylesheet" type="text/css" />
@@ -15,7 +13,7 @@
         @component('themes.bootstrap.elements.portlets.portlet', ['icon' => 'icon-book-open', 'title' => 'Formulario de registro de eventos: '])
             <div class="row">
                 <div class="col-md-7 col-md-offset-2">
-                    {!! Form::open (['id'=>'form_eventos','method'=>'POST', 'route'=> ['talento.humano.evento.store']]) !!}
+                    {!! Form::model ($evento,['id'=>'form_eventos','method'=>'PUT', 'route'=> ['talento.humano.evento.update', $evento->PK_EVNT_IdEvento],'role'=>"form"]) !!}
                     {!! Field::textarea(
                             'EVNT_Descripcion',
                             ['label' => 'Descripción del evento:', 'required', 'auto' => 'off', 'max' => '300', "rows" => '4'],
@@ -29,32 +27,14 @@
                             ['label'=>'Hora:','class' => 'timepicker timepicker-no-seconds', 'data-date-format' => "hh/mm-", 'data-date-start-date' => "+0d", 'required', 'auto' => 'off'],
                             ['help' => 'Selecciona la hora.', 'icon' => 'fa fa-clock-o']) !!}
 
-                    <br><br><br><br>
-                    @component('themes.bootstrap.elements.tables.datatables', ['id' => 'lista-empleados'], ['class'=>'display'])
-                        @slot('columns', [
-                            '#',
-                            'Nombres',
-                            'Apellidos',
-                            'Cédula',
-                            'Email',
-                            'Rol ',
-                            'Área',
-                            'Acciones'
-                        ])
-                    @endcomponent
-
-
                     <div class="form-actions">
                         <div class="row">
                             <div class=" col-md-offset-4">
-                                {!! Form::submit('Registrar',['class' => 'btn blue']) !!}
-                                {!! Form::reset('Cancelar', ['class' => 'btn btn-danger']) !!}
+                                {!! Form::submit('Editar',['class' => 'btn blue']) !!}
                             </div>
                         </div>
                     </div>
-
                     {!! Form::close() !!}
-                    <button id="button">Ver</button>
                 </div>
             </div>
     </div>
@@ -62,9 +42,6 @@
 
 @endsection
 @push('plugins')
-<script src="{{ asset('assets/global/scripts/datatable.js') }}" type="text/javascript"></script>
-<script src="{{ asset('assets/global/plugins/datatables/datatables.min.js') }}" type="text/javascript"></script>
-<script src="{{ asset('assets/global/plugins/datatables/plugins/bootstrap/datatables.bootstrap.js') }}" type="text/javascript"></script>
 <script src="{{ asset('assets/global/plugins/bootstrap-maxlength/bootstrap-maxlength.min.js') }}" type="text/javascript"></script>
 <script src="{{ asset('assets/global/plugins/jquery-validation/js/jquery.validate.min.js') }}" type="text/javascript"></script>
 <script src="{{ asset('assets/global/plugins/jquery-validation/js/additional-methods.min.js') }}" type="text/javascript"></script>
@@ -80,157 +57,14 @@
             @if(Session::has('message'))
     var type="{{Session::get('alert-type','info')}}"
     switch(type){
-        case 'success':
+        case 'info':
             toastr.options.closeButton = true;
-            toastr.success("{{Session::get('message')}}",'Creación de evento exitoso:');
+            toastr.info("{{Session::get('message')}}",'Modificación exitosa:');
             break;
     }
-    @endif
-jQuery(document).ready(function () {
-
-        var  url;
-        // table = $('#lista-empleados');
-        url = "{{ route('talento.humano.tablaEmpleados')}}";
-        $.fn.dataTable.ext.errMode = 'throw';
-        /*/para que no le salga errores al funcionario*/
+            @endif
 
 
-        var table=$('#lista-empleados').DataTable({
-
-            lengthMenu: [
-                [5, 10, 25, 50, -1],
-                [5, 10, 25, 50, "Todo"]
-            ],
-            responsive: true,
-            colReorder: false,
-            processing: true,
-            serverSide: false,
-            // select: {style: 'multi'
-            // },
-            ajax: url,
-            language: {
-                "sProcessing": '<i class="fa fa-spinner fa-spin fa-3x fa-fw"></i> <span class="sr-only">Procesando...</span>',
-                "sLengthMenu": "Mostrar _MENU_ registros",
-                "sZeroRecords": "No se encontraron resultados",
-                "sEmptyTable": "Ningún dato disponible en esta tabla",
-                "sInfo": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
-                "sInfoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
-                "sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
-                "sInfoPostFix": "",
-                "sSearch": "Buscar:",
-                "sUrl": "",
-                "sInfoThousands": ",",
-                "sLoadingRecords": "Cargando...",
-                "oPaginate": {
-                    "sFirst": "Primero",
-                    "sLast": "Último",
-                    "sNext": "Siguiente",
-                    "sPrevious": "Anterior"
-                },
-                "oAria": {
-                    "sSortAscending": ": Activar para ordenar la columna de manera ascendente",
-                    "sSortDescending": ": Activar para ordenar la columna de manera descendente"
-                }
-            },
-            columns: [
-
-                {data: 'DT_Row_Index'},
-                {data: 'PRSN_Nombres', name: 'Nombres'},
-                {data: 'PRSN_Apellidos', name: 'Apellidos'},
-                {data: 'PK_PRSN_Cedula', name: 'Cédula'},
-                {data: 'PRSN_Correo', name: 'Correo Electronico'},
-                {data: 'PRSN_Rol', name: 'Rol'},
-                {data: 'PRSN_Area', name: 'Área'},
-                //'<a href="javascript:;" " class="btn blue"><i class="fa fa-check"></i></a>',
-
-                {
-                    data: "PK_PRSN_Cedula",
-                    name: 'action',
-                    title: 'Acciones',
-                    orderable: false,
-                    searchable: false,
-                    exportable: false,
-                    printable: false,
-                    className: '',
-                    render: function (data, type, full, meta) {
-                        return '<a href="javascript:;" " class="btn blue"><i class="fa fa-check"></i></a>';
-                    },
-                    responsivePriority: 2
-                }
-            ],
-            buttons: [
-                {
-                    extend: 'colvis',
-                    className: 'btn btn-circle btn-icon-only btn-default tooltips t-colvis',
-                    text: '<i class="fa fa-bars"></i>'
-                },
-                {
-                    text: '<i class="fa fa-refresh"></i>',
-                    className: 'btn btn-circle btn-icon-only btn-default tooltips t-refresh',
-                    action: function (e, dt, node, config) {
-                        dt.ajax.reload();
-                    }
-                }
-
-            ],
-            pageLength: 10,
-            dom: "<'row' <'col-md-12'B>><'row'<'col-md-6 col-sm-12'l><'col-md-6 col-sm-12'f>r><'table-scrollable't><'row'<'col-md-5 col-sm-12'i><'col-md-7 col-sm-12'p>>",
-
-        });
-        $('#lista-empleados tbody').on( 'click', 'tr', function () {
-            $(this).toggleClass('selected');
-
-        } );
-        $('#button').click( function () {
-            var data = table.row('.selected').data();
-            alert( 'You clicked on :  '+data['PK_PRSN_Cedula']);
-        } );
-
-
-        var handleTooltips = function () {
-            $('.t-print').attr({'data-container': "body", 'data-placement': "top", 'data-original-title': "Imprimir"});
-            $('.t-copy').attr({
-                'data-container': "body",
-                'data-placement': "top",
-                'data-original-title': "Copiar al portapales"
-            });
-            $('.t-pdf').attr({
-                'data-container': "body",
-                'data-placement': "top",
-                'data-original-title': "Exportar a PDF"
-            });
-            $('.t-excel').attr({
-                'data-container': "body",
-                'data-placement': "top",
-                'data-original-title': "Exportar a EXCEL"
-            });
-            $('.t-csv').attr({
-                'data-container': "body",
-                'data-placement': "top",
-                'data-original-title': "Exportar a CSV"
-            });
-            $('.t-colvis').attr({
-                'data-container': "body",
-                'data-placement': "top",
-                'data-original-title': "Mostrar/Ocultar Columnas"
-            });
-            $('.t-refresh').attr({
-                'data-container': "body",
-                'data-placement': "top",
-                'data-original-title': "Recargar"
-            });
-
-            $('.tooltips').tooltip();
-        }
-
-        jQuery(document).ready(function () {
-            handleTooltips();
-        })
-    });
-</script>
-@endpush
-@push('functions')
-<script>
     var ComponentsDateTimePickers = function () {
         var handleTimePickers = function () {
 
