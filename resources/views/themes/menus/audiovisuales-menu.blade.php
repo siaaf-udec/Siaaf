474adfc5ab6b9ca67377032d051e2c7a95044f84
Jerson Gutiérrez <jersongutierrez1@gmail.com>
2017-07-26 23:07:09 -0500

    Correciones Audivisuales

--- resources/views/themes/menus/audiovisuales-menu.blade.php
+++ resources/views/themes/menus/audiovisuales-menu.blade.php
@@ -10,9 +10,26 @@
         <li class="nav-item {{ active(['funcionario.index'], 'start active open') }}">
             <a href="{{ route('funcionario.index') }}" class="nav-link">
                 <i class="fa fa-users"></i>
-                <span class="title">Gestionar Funcionarios</span>
+                <span class="title">Gestion Funcionarios</span>
             </a>
         </li>
-       
+        <li class="nav-item {{ active(['audiovisuales.administrador.index'], 'start active open') }}">
+            <a href="{{ route('audiovisuales.administrador.index') }}" class="nav-link">
+                <i class="fa fa-users"></i>
+                <span class="title">Gestion Administradores</span>
+            </a>
+        </li>
+        <li class="nav-item {{ active(['audiovisuales.articulo.index'], 'start active open') }}">
+            <a href="{{ route('audiovisuales.articulo.index') }}" class="nav-link">
+                <i class="fa fa-users"></i>
+                <span class="title">Gestion Articulos</span>
+            </a>
+        </li>
+        <li class="nav-item {{ active(['audiovisuales.superAdmin.index'], 'start active open') }}">
+            <a href="{{ route('audiovisuales.superAdmin.index') }}" class="nav-link">
+                <i class="fa fa-users"></i>
+                <span class="title">Gestion SuperAdmin</span>
+            </a>
+        </li>         
     </ul>
 </li>
