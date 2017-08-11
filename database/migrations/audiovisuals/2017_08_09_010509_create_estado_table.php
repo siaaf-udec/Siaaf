a0b57f4afda6ad6955740ffc02efdd4fa0f9ab55
Jerson Gutiérrez <jersongutierrez1@gmail.com>
2017-08-11 00:31:28 -0500

    DB audiovisuals

---
+++ database/migrations/audiovisuals/2017_08_09_010509_create_estado_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEstadoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection('audiovisuals')->create('TBL_Estados', function (Blueprint $table) {
+            $table->increments('id');
+            $table->String('EST_Descripcion');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('TBL_Estados');
+    }
+}
---
+++ database/migrations/audiovisuals/2017_08_09_010709_create_kits_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection('audiovisuals')->create('TBL_Kits', function (Blueprint $table) {
+            $table->increments('id');
+            $table->String('KIT_Nombre');
+
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('TBL_Kits');
+    }
+}
