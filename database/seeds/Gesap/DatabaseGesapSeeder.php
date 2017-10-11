2ebd42fad6f59db8fccc7fefac42980786495e1e
Dalaven Uind <dalaven1996@yahoo.com>
2017-10-11 09:55:05 -0500

    Correccion Seeder

--- database/seeds/Gesap/DatabaseGesapSeeder.php
+++ database/seeds/Gesap/DatabaseGesapSeeder.php
@@ -13,17 +13,10 @@ class DatabaseGesapSeeder extends Seeder
     {
         $this->call(RoleGesapSeeder::class);
         $this->call(UsersGesapTableSeeder::class);
-<<<<<<< Updated upstream
         $this->call(AnteproyectosSeeder::class);
         $this->call(RadicacionSeeder::class);
         $this->call(EncargadosSeeder::class);
         $this->call(PermissionGesapSeeder::class);
-=======
-        //$this->call(AnteproyectosSeeder::class);
-      //  $this->call(RadicacionSeeder::class);
-      //  $this->call(EncargadosSeeder::class);
-      //  $this->call(RoleGesapSeeder::class);
-     //   $this->call(PermissionGesapSeeder::class);
->>>>>>> Stashed changes
+
     }
 }
--- database/seeds/Gesap/RoleGesapSeeder.php
+++ database/seeds/Gesap/RoleGesapSeeder.php
@@ -9,18 +9,11 @@ class RoleGesapSeeder extends Seeder
     public function run()
     {
         Role::insert([
-<<<<<<< Updated upstream
             ['name'=>'Administrator_Gesap','display_name'=>'Administrador','description'=>'--'],
             ['name'=>'Coordinator_Gesap','display_name'=>'Coordinador','description'=>'--'],
             ['name'=>'Evaluator_Gesap','display_name'=>'Docente','description'=>'--'],
             ['name'=>'Student_Gesap','display_name'=>'Estudiante','description'=>'--']
-=======
-            ['name'=>'Coordinator_uni','display_name'=>'Coordinador','description'=>'--'],
-            ['name'=>'Funcionario_uni','display_name'=>'Funcionario','description'=>'--'],
-            ['name'=>'Pasante_uni','display_name'=>'Pasante','description'=>'--'],
-            ['name'=>'Empresario_uni_Gesap','display_name'=>'Empresario','description'=>'--'],
-            
->>>>>>> Stashed changes
+
         ]);
     }
 }
