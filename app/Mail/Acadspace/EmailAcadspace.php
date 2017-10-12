928aecf4f0f9df04eabb6bf36dd224fdc9e395b9
Jorge Poveda <jalexispoveda010@gmail.com>
2017-10-12 00:35:48 -0500

    Mail acadspace
    envio de correo espacios academicos

--- app/Mail/Acadspace/EmailAcadspace.php
+++ app/Mail/Acadspace/EmailAcadspace.php
@@ -2,12 +2,13 @@
 
 namespace App\Mail\Acadspace;
 
+use Exception;
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class EmailAcadspace extends Mailable
+class EmailAcadspace extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
@@ -17,25 +18,23 @@ class EmailAcadspace extends Mailable
      * @var int
      */
     public $tries = 3;
+
     /**
      * @var
      */
-
     public $message;
-    public $asunto;
-    public $file;
+    public $subject;
 
     /**
      * Create a new message instance.
      *
-     * @param $asunto
+     * @param $subject
      * @param $message
-     *
-     * @return void
      */
-    public function __construct($asunto, $message)
+    public function __construct($subject, $message)
     {
-        $this->asunto = $asunto;
+
+        $this->subject = $subject;
         $this->message = $message;
     }
 
@@ -46,12 +45,32 @@ class EmailAcadspace extends Mailable
      */
     public function build()
     {
-
-            return $this->from(['address' => 'no-reply@ucundinamarca.edu.co', 'name' => env('APP_NAME')])
-                ->view('acadspace.formatosacademicos.emailFormatos',['title'=>$this->asunto, 'body' => $this->message]);
-
-
+        return $this->from(['address' => 'no-reply@ucundinamarca.edu.co', 'name' => env('APP_NAME')])
+            ->with([
+                'subject' => $this->subject,
+                'message' => $this->message,
+            ])
+            ->view('acadspace.formatosacademicos.enviomailformatos');
     }
 
+    /**
+     * The job failed to process.
+     *
+     * @param  Exception  $exception
+     * @return void
+     */
+    public function failed(Exception $exception)
+    {
+        // Send user notification of failure, etc...
+    }
 
+    /**
+     * Determine the time at which the job should timeout.
+     *
+     * @return \DateTime
+     */
+    public function retryUntil()
+    {
+        return now()->addSeconds(5);
+    }
 }
