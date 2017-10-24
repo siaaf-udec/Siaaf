6c132f4f0c9dfa7d2b113b19e0fbc8690c4abedb
Unknown <jesus.andres1996@hotmail.com>
2017-10-23 20:14:03 -0500

    Comentarios controlador de alertas interaccion

--- app/Container/Unvinteraction/src/Controllers/Controller_Alertas.php
+++ app/Container/Unvinteraction/src/Controllers/Controller_Alertas.php
@@ -1,8 +1,5 @@
 <?php
-
 namespace App\Container\Unvinteraction\src\Controllers;
-
-
 use App\Container\Unvinteraction\src\TBL_Usuarios;
 use App\Container\Unvinteraction\src\TBL_Tipo_Usuario;
 use App\Container\Unvinteraction\src\TBL_Estado_Usuario;
@@ -22,10 +19,8 @@ use App\Container\Unvinteraction\src\TBL_Documentacion_Extra;
 use App\Container\Unvinteraction\src\TBL_Notificaciones;
 use App\Container\Users\Src\Interfaces\UserInterface;
 use App\Container\Users\Src\User;
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-
 use Yajra\DataTables\DataTables;
 use Illuminate\Support\Facades\Storage;
 use App\Http\Controllers\Controller;
@@ -41,7 +36,10 @@ class Controller_Alertas extends Controller
      * @return \Illuminate\Http\Response
      */
     private $path='unvinteraction';
-    
+    /*funcion para crear  una alerta de los convenios que van a finalizar para el usuario
+    *@param \Illuminate\Http\Request
+    *@return \Illuminate\Http\Response
+    */
     public function Alerta(Request $request)
     {
         $estado=1;
@@ -51,6 +49,7 @@ class Controller_Alertas extends Controller
             ->select('TBL_Convenios.PK_Convenios', 'TBL_Convenios.Nombre', 'TBL_Convenios.Fecha_Inicio', 'TBL_Convenios.Fecha_Fin', 'TBL_Estado.Estado', 'TBL_Sede.Sede')
             ->where('TBL_Participantes.FK_TBL_Usuarios', '=', $request->user()->identity_no)
             ->get();
+        //calculo de la diferencia de fecha  para saber si hay que crear la  alerta
         $carbon = new \Carbon\Carbon();
         foreach ($Convenio as $row) {
             $dtVancouver = $carbon->now();
@@ -58,10 +57,10 @@ class Controller_Alertas extends Controller
             $diferencia = $fecha->diffInDays($dtVancouver,false);
             $diferencia;
             if($estado == 1 && $diferencia >= 0  ){
-                
-            } else {
+                //si el convenio ya finalizo no se envia ninguna  alerta
+             } else {
                 if($estado == 1 && $diferencia >= -60 ){
-                   $notificacion = new TBL_Notificaciones();
+                    $notificacion = new TBL_Notificaciones();
                     $notificacion->Titulo='Finalizacion convenio '.$row->Nombre;
                     $notificacion->Mensaje='El siguiente mensaje es para avisar que el convenio '.$row->Nombre.' en el cual se encuentra como participante esta a punto de finalizar, porfavor realizar las respectivas evaluaciones';
                     $notificacion->Bandera = 'NO VISTO';
@@ -71,23 +70,31 @@ class Controller_Alertas extends Controller
             } 
         }
         return view($this->path.'.Listar_Notificaciones');
-        
     }
+    /*funcion para la vista de la alertas en ajax
+    *@param \Illuminate\Http\Request
+    *@return \Illuminate\Http\Response
+    */
     public function Alerta_Ajax(Request $request)
     {
-        
         return view($this->path.'.Listar_Notificaciones_Ajax');
-        
     }
-     public function Listar_Alerta(Request $request)
+    /*funcion para envio de los datos para la tabla de datos
+    *@param \Illuminate\Http\Request
+    *@return Yajra\DataTables\DataTable
+    */
+    public function Listar_Alerta(Request $request)
     {
           $Notificaciones = TBL_Notificaciones::select('PK_Notificacion','Titulo', 'Bandera')
               ->where('bandera','NO VISTO')
               ->where('FK_TBL_Usuarios',$request->user()->identity_no)
               ->get();
         return Datatables::of($Notificaciones)->addIndexColumn()->make(true);
-         //return $Notificaciones;
     }
+    /*funcion para la vista completa del mensaje de la alertas en ajax
+    *@param int id
+    *@return \Illuminate\Http\Response
+    */
     public function Ver_Alerta($id)
     {
         $Bandera = TBL_Notificaciones::findOrFail($id);
@@ -96,15 +103,12 @@ class Controller_Alertas extends Controller
         $Notificacion = TBL_Notificaciones::findOrFail($id);
         return view($this->path.'.Ver_Notificacion', compact('Notificacion'));
     }
+    /*funcion prueba 
+    *
+    *@return \Illuminate\Http\Response
+    */
     public function Reporte()
     {
-      
         return view($this->path.'.Ver_Reporte');
     }
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-   
 }
