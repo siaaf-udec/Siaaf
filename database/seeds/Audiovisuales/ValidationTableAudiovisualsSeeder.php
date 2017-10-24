7ab3c1eec34d9b7b1f6a4111ad2ca443ed67d3b1
Unknown <cristianjojoa01@gmail.com>
2017-10-24 16:38:04 -0500

    Nuevas Validaciones
    Audiovisuales

--- database/seeds/Audiovisuales/ValidationTableAudiovisualsSeeder.php
+++ database/seeds/Audiovisuales/ValidationTableAudiovisualsSeeder.php
@@ -16,16 +16,19 @@ class ValidationTableAudiovisualsSeeder extends Seeder
     public function run()
     {
         $preguntas = [
-            ['VAL_PRE_Nombre' => ' ¿Numero de Prestamos y Reservas que puede realizar el Usuario?', 'VAL_PRE_Valor' => '1' ],
-            ['VAL_PRE_Nombre' => ' ¿Maximo de horas que pueden reservar un articulo?', 'VAL_PRE_Valor' => '1' ],
-            ['VAL_PRE_Nombre' => ' ¿Numero de kits que puede Reservar o Prestar por Usuario?', 'VAL_PRE_Valor' => '1' ],
-            ['VAL_PRE_Nombre' => ' ¿Numero de articulos que puede Reservar o Prestar por Usuario?	', 'VAL_PRE_Valor' => '1' ],
-            ['VAL_PRE_Nombre' => ' ¿Numero de dias que puede Reservar?', 'VAL_PRE_Valor' => '1' ],
-            ['VAL_PRE_Nombre' => ' ¿Numero de horas para cancelar un Prestamo?', 'VAL_PRE_Valor' => '1' ],
-            ['VAL_PRE_Nombre' => ' ¿Dias de anticipación para Reservar?	', 'VAL_PRE_Valor' => '1' ],
-            ['VAL_PRE_Nombre' => ' ¿Cantidad de Tipos de articulo repetidos que puede reservar el usuario?', 'VAL_PRE_Valor' => '1' ],
+            ['VAL_PRE_Nombre' => ' ¿Número de préstamos que puede realizar el usuario?', 'VAL_PRE_Valor' => '1' ],
+            ['VAL_PRE_Nombre' => ' ¿Número de reservas que puede realizar el usuario?', 'VAL_PRE_Valor' => '1' ],
+            ['VAL_PRE_Nombre' => ' ¿Máximo de horas que pueden reservar un artículo?', 'VAL_PRE_Valor' => '1' ],
+            ['VAL_PRE_Nombre' => ' ¿Máximo de días que pueden reservar un artículo?', 'VAL_PRE_Valor' => '1' ],
+            ['VAL_PRE_Nombre' => ' ¿Máximo de horas que pueden reservar un kit? ', 'VAL_PRE_Valor' => '1' ],
+            ['VAL_PRE_Nombre' => ' ¿Máximo de días que pueden reservar un kit? ', 'VAL_PRE_Valor' => '1' ],
+            ['VAL_PRE_Nombre' => ' ¿Número de artículos que puede Reservar o Prestar por Usuario?	', 'VAL_PRE_Valor' => '1' ],
+            ['VAL_PRE_Nombre' => ' ¿Número de horas para cancelar una Reserva?', 'VAL_PRE_Valor' => '1' ],
             ['VAL_PRE_Nombre' => ' ¿Numero de horas maximas para articulo con tiempo libre?', 'VAL_PRE_Valor' => '1' ],
             ['VAL_PRE_Nombre' => ' ¿Numero de horas maximas para articulo con tiempo asignado?', 'VAL_PRE_Valor' => '1' ],
+            ['VAL_PRE_Nombre' => ' ¿Días de anticipación para Reservar?	', 'VAL_PRE_Valor' => '1' ],
+            ['VAL_PRE_Nombre' => ' ¿Cantidad de Tipos de articulo repetidos que puede reservar el usuario?', 'VAL_PRE_Valor' => '1' ],
+            ['VAL_PRE_Nombre' => ' ¿Numero de dias habiles para escoger fecha de reserva?', 'VAL_PRE_Valor' => '1' ],
         ];
 
         foreach ($preguntas as $pregunta ) {
