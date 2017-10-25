445290a632f5262619391f83e7348b06375c56ec
Unknown <zerocool@null.net>
2017-10-25 15:24:34 -0500

    Carpark: Corrección Correos

--- app/Container/Carpark/src/Mail/EmailCarpark.php
+++ app/Container/Carpark/src/Mail/EmailCarpark.php
@@ -2,12 +2,13 @@
 
 namespace App\Container\Carpark\src\Mail;
 
+use Exception;
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class EmailCarpark extends Mailable
+class EmailCarpark extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
@@ -21,18 +22,17 @@ class EmailCarpark extends Mailable
      * @var
      */
 
-    public $asunto;
+    public $nombre;
 
     /**
      * Create a new message instance.
      *
-     * @param $asunto
-     * @param $message
+     * @param $nombre
      *
      */
-    public function __construct($asunto)
+    public function __construct($nombre)
     {
-        $this->asunto = $asunto;
+        $this->nombre = $nombre;
     }
 
     /**
@@ -43,7 +43,14 @@ class EmailCarpark extends Mailable
     public function build()
     {
         $cuerpoMensaje = 'Advertencia, recuerda que el parqueadero de la Universidad De Cundinamarca cierra a las 22 horas, así que es necesario que por favor saques tu vehículo de allí o se procederá a tomar acciones';
-        return $this->from(['address' => 'no-reply@ucundinamarca.edu.co', 'name' => env('APP_NAME')])->view('carpark.correos.plantilla', ['title' => $this->asunto, 'body' => $cuerpoMensaje]);        
+
+        return $this->from(['address' => 'no-reply@ucundinamarca.edu.co', 'name' => 'Cierre Parqueader UdeC'])
+            ->with([
+                'subject' => $this->nombre.': Advertencia Cierre De Parqueadero UdeC',
+                'title' => $this->nombre.': Advertencia Cierre De Parqueadero UdeC',
+                'body' => $cuerpoMensaje
+            ])
+            ->view('carpark.correos.plantilla');      
 
     }
 
