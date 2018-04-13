9caf2893ad938dcfc32ddfa1142aa196956c35d0
FredyHenao <jhongallego45y@hotmail.com>
2018-04-13 08:05:19 -0500

    Error Correction

--- database/seeds/Adminregist/DatabaseAdminRegistSeeder.php
+++ database/seeds/Adminregist/DatabaseAdminRegistSeeder.php
@@ -12,27 +12,7 @@ class DatabaseAdminRegistSeeder extends Seeder
      */
     public function run()
     {
-<<<<<<< Updated upstream:database/seeds/Adminregist/DatabaseAdminRegistSeeder.php
         $this->call(NovedadesSeeder::class);
         $this->call(PermissionsAdminRegist::class);
-=======
-        $kits = [
-
-            ['KIT_Nombre' => 'Ninguno'],
-            ['KIT_Nombre' => 'Kit 1'],
-            ['KIT_Nombre' => 'Kit 2'],
-            ['KIT_Nombre' => 'Kit 3'],
-
-        ];
-
-        foreach ($kits as $kit) {
-            $aux = new Kit();
-            $aux->KIT_Nombre = $kit['KIT_Nombre'];
-            $aux->KIT_FK_Estado_id = 1;
-            $aux->KIT_FK_Tiempo = 3;
-            $aux->KIT_Codigo = 1234;
-            $aux->save();
-        }
->>>>>>> Stashed changes:database/seeds/Audiovisuales/KitsTableAudiovisualsSeeder.php
     }
 }
--- resources/views/material/partials/head.blade.php
+++ resources/views/material/partials/head.blade.php
@@ -18,7 +18,7 @@
 <meta name="theme-color" content="#ffffff">
 {{-- ENDS FAVICONS --}}
 {{-- BEGIN GLOBAL MANDATORY STYLES --}}
-
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css" />
 <link href="{{ asset('assets/global/plugins/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet" type="text/css" />
 <link href="{{ asset('assets/global/plugins/simple-line-icons/simple-line-icons.min.css') }}" rel="stylesheet" type="text/css" />
 <link href="{{ asset('assets/global/plugins/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
