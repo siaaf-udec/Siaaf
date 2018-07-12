90c6c2bcfaebbaa89e52c84f3dc08d7ec4017b20
DIEGO <ferchobmg1107@gmail.com>
2018-07-12 09:12:23 -0500

    Relacion entre ingresos y usuarios  carpark

--- app/Container/Carpark/src/Ingresos.php
+++ app/Container/Carpark/src/Ingresos.php
@@ -4,6 +4,7 @@ namespace App\Container\Carpark\src;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Container\Users\src\UsersUdec;
+use App\Container\Carpark\src\Usuarios;
 
 class Ingresos extends Model
 {
@@ -21,8 +22,15 @@ class Ingresos extends Model
         'CI_CodigoMoto'];
 
     //Función de conexión entre las tablas de Ingresos y Usuarios por el campo de CI_CodigoUser y PK_CU_Codigo para realizar las busquedas complementarias
-    public function relacionIngresosUsuarios()
+    // public function relacionIngresosUsuarios()
+    // {
+    //     return $this->belongsTo(UsersUdec::class, 'CI_CodigoUser');
+    // }
+
+
+     public function relacionIngresosUsuarios()
     {
-        return $this->belongsTo(UsersUdec::class, 'CI_CodigoUser');
+        //return $this->belongsTo(UsersUdec::class, 'FK_CM_CodigoUser');
+        return $this->belongsTo(Usuarios::class, 'CU_Cedula','CI_CodigoUser');
     }
 }
