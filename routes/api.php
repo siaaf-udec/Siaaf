41a4509a88a09d58e360cc0e35228a2352403bbf
DIEGO <ferchobmg1107@gmail.com>
2018-09-14 09:32:27 -0500

    Correcion api

--- routes/api.php
+++ routes/api.php
@@ -15,4 +15,7 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+
+Route::get('app/Container/Acadspace/src/Controllers/api-rest-app/listado.php); 
+
 });
