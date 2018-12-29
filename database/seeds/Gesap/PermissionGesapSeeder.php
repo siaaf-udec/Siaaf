9c98f06f54e91244e0714641ed1e450cb6ab9109
Unknown <alejoespi1007@gmail.com>
2018-12-28 21:20:58 -0500

    Correcion a la base de datos GESAP
    Cambio de algunos tipo de variables en diferentes tablas de la base de datos

--- database/seeds/Gesap/PermissionGesapSeeder.php
+++ database/seeds/Gesap/PermissionGesapSeeder.php
@@ -9,42 +9,58 @@ class PermissionGesapSeeder extends Seeder
 
     public function run()
     {
+
+        //Inicio para los permisos generales de los Roles
+
+        $permissionAdmin = new Permission;
+        $permissionAdmin->name = 'ADMIN_GESAP';
+        $permissionAdmin->display_name = 'Gesao';
+        $permissionAdmin->description = 'Acceso completo al modulo de gesap.';
+        $permissionAdmin->module_id = 7;
+        $permissionAdmin->save();
+
         $permissionFuncionario = new Permission;
         $permissionFuncionario->name = 'GESAP_MODULE';
         $permissionFuncionario->display_name = 'Modulo Gesap';
         $permissionFuncionario->description = 'Acceso a solo este modulo';
         $permissionFuncionario->module_id = 7;
         $permissionFuncionario->save();
+
         $permissionFuncionario = new Permission;
         $permissionFuncionario->name = 'LIST_ANTEPROYECTOS';
         $permissionFuncionario->display_name = 'Lista de Anteproyectos';
         $permissionFuncionario->description = 'Acceso a los anteproyectos abiertos para su realizacion';
         $permissionFuncionario->module_id = 7;
         $permissionFuncionario->save();
+
         $permissionFuncionario = new Permission;
         $permissionFuncionario->name = 'LIST_PROYECTOS';
         $permissionFuncionario->display_name = 'Lista de Proyectos';
         $permissionFuncionario->description = 'Acceso a los Proyectos que ya fueron anteriormente aprovados';
         $permissionFuncionario->module_id = 7;
         $permissionFuncionario->save();
+
         $permissionFuncionario = new Permission;
         $permissionFuncionario->name = 'ADD_USER_GESAP';
         $permissionFuncionario->display_name = 'Listar Usuarios Gesap';
         $permissionFuncionario->description = 'Ver usuarios de Gesap';
         $permissionFuncionario->module_id = 7;
         $permissionFuncionario->save();
+
         $permissionFuncionario = new Permission;
         $permissionFuncionario->name = 'FIND_DB_GESAP';
         $permissionFuncionario->display_name = 'Usuarios registrados';
         $permissionFuncionario->description = 'Acceso a los usuarios que se han registrado.';
         $permissionFuncionario->module_id = 7;
         $permissionFuncionario->save();
+
         $permissionFuncionario = new Permission;
         $permissionFuncionario->name = 'REPORT_GESAP';
         $permissionFuncionario->display_name = 'Reportes Gesap';
         $permissionFuncionario->description = 'Hacer Reportes por diferentes categorias de Gesap';
         $permissionFuncionario->module_id = 7;
         $permissionFuncionario->save();
+
         $permissionFuncionario = new Permission;
         $permissionFuncionario->name = 'GRAPHICS_GESAP';
         $permissionFuncionario->display_name = 'Graficos Gesap';
@@ -52,12 +68,20 @@ class PermissionGesapSeeder extends Seeder
         $permissionFuncionario->module_id = 7;
         $permissionFuncionario->save();
 
-            
-            
-            /* fin generales*/
-           
         
+        //Fin de permisos generales para los roles
 
+        //Inicio de permisos para el CRUD de Admin
+
+        $permissionFuncionario = new Permission;
+        $permissionFuncionario->name = 'ADD_ANTEPROYECTO';
+        $permissionFuncionario->display_name = 'Anteproyecto Gesap';
+        $permissionFuncionario->description = 'Permiso para el admin de registrar un anteproyecto';
+        $permissionFuncionario->module_id = 7;
+        $permissionFuncionario->save();
+            
+        //Fin de permisos para el CRUD de Admin
+        
         
     }
 }
