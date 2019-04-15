78c238ea45a63335d1cdbb2796f9602f1bc0cf28
Alejandro Espitia <alejoespi1007@gmail.com>
2019-04-15 18:45:40 -0500

    tilde x2

--- resources/views/gesap/Coordinador/Usuarios.blade.php
+++ resources/views/gesap/Coordinador/Usuarios.blade.php
@@ -175,7 +175,7 @@
                             }
                         });
                     } else {
-                        swal("Cancelado", "No se Deshabilitó ningun usuario", "error");
+                        swal("Cancelado", "No se Deshabilitó ningún usuario", "error");
                     }
                 });
 
